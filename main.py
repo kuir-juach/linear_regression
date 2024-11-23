ac3bb32d735e41dd26a3b45bfd07f5832b7ae8c9
kuir-juach <k.thuch@alustudent.com>
2024-11-23 20:18:08 +0200

    update

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ import logging
 app = FastAPI()
 
 # Configure CORS middleware
-origins = [
-    "http://localhost",         
-    "http://localhost:3000",    
-    "http://127.0.0.1:8000",    
-    "*"                         
-]
+#origins = [
+ #   "http://localhost",         
+  #  "http://localhost:3000",    
+   # "http://127.0.0.1:8000",    
+    #"*"                         
+#]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=origins, # type: ignore
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
